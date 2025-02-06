1ae6f8430d452ac8ee7f537d0ec9c18663a92337
Joshua Burns <joshua.burns@liatrio.com>
2025-02-06 08:40:35 -0800

    chore: initial commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+.PHONY: demo demo-magic
+
+default: demo
+
+demo-magic: demo-magic.sh
+
+demo-magic.sh:
+	curl -s https://raw.githubusercontent.com/paxtonhare/demo-magic/refs/heads/master/demo-magic.sh -o demo-magic.sh
+
+demo: demo-magic
+	./demo.sh
---
+++ demo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+########################
+# include the magic
+########################
+. demo-magic.sh
+
+# hide the evidence
+clear
+
+# Demo Magic Commands Summary:
+# pe "command" - Print and Execute: Simulates typing the command, waits for ENTER, then executes it.
+# pei "command" - Print and Execute Immediately: Simulates typing the command, then executes it immediately.
+# p "command" - Print Only: Simulates typing the command, waits for ENTER, but does not execute it.
+# wait - Waits for the user to press ENTER. If PROMPT_TIMEOUT is set, waits for the specified time.
+# cmd - Enters interactive mode, allowing newly typed commands to be executed within the script.
+# repl - Enters REPL mode, allowing newly typed commands to be executed within the script. Type 'exit' to leave REPL mode.
+
+# Example usage of pe command
+pe "bat Makefile"
